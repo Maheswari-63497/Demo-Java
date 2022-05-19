77f5c8da4fa5d1e752ee5405359285af6e4d47af
Maheswari.S <maheswaris@hexaware.com>
2022-05-19 09:51:11 +0000

    Updated the Java file

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -7,10 +7,20 @@ import javax.servlet.http.*;
 public class Hello extends HttpServlet {
 
    private String message;
-   @Override
+   
    public void init() throws ServletException {
       // Do required initialization
       message = "Hello World: src/main/java/Hello.java";
-      System.out.println(message);
+   }
+
+   public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+      // Set response content type
+      response.setContentType("text/html");
+
+      // Actual logic goes here.
+      PrintWriter out = response.getWriter();
+      out.println("<h1>" + message + "</h1>");
    }
 }
